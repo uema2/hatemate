3243957ac87434e2f82277a258f0685cb84b28ad
taro.tanaka <sub.tanaka2@gmail.com>
2019-02-08 10:58:50 +0000

    user registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "toppages#index"
   
   get "about", to: "toppages#show"
+  get "signup", to: "users#new"
+  resources :users, only: [:show, :new, :create]
 end
